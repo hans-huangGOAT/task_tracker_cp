815ef417aa48eb1f172999e6309f16e9ad069ef7
Hans <fb_hans@outlook.com>
2021-01-24 22:02:21 +0800

    Project Finished

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,18 +1,40 @@
-const AddTask = () => {
+import { useState } from 'react'
+
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert("Please add a task")
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
-                <lable>Task</lable>
-                <input type='text' placeholder='Add Task...' />
+                <label>Task</label>
+                <input type='text' value={text} onChange={(e) => setText(e.target.value)} placeholder='Add Task...' />
             </div>
             <div className='form-control'>
-                <lable>Day & Time</lable>
-                <input type='text' placeholder='Add Day...' />
+                <label>Day  Time</label>
+                <input type='text' value={day} onChange={(e) => setDay(e.target.value)} placeholder='Add Day...' />
             </div>
-            <div className='form-control'>
-                <lable>Set Reminder</lable>
-                <input type='checkbox' />
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' checked={reminder} onChange={(e) => setReminder(e.target.checked)} />
             </div>
+
+            <input type='submit' value='Save Task' className='btn btn-block' />
         </form>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import {useLocation} from 'react-router-dom'
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({ title,onAdd,showAddTask }) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            {location.pathname === '/' && <Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={onAdd} />}
         </header>
     )
 }
